a5cacdf114d55d290e0eca1bf3fd602fdaf00360
vidvancheripalli-ui <vidvancheripalli@gmail.com>
2025-10-10 00:07:32 +0530

    Update WebConfig.java

--- src/main/java/com/aitutor/trail/config/WebConfig.java
+++ src/main/java/com/aitutor/trail/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://tutor-nexus-96.vercel.app")
+                        .allowedOrigins("https://ai-tutor-app-kappa.vercel.app")
                         .allowedMethods("*");
             }
         };
